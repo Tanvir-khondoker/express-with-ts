2e877bfe6f1abd625a2bb1458141a7711d43c5c7
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2025-03-02 12:33:45 +0600

    before adding any eslint, prettier disasar

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application } from "express";
+import cors from "cors";
+
+const app : Application = express();
+
+// parsers
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+
+
+export default app;
+
